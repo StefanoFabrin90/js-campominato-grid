30844b102a48b9a152339a786ff7d4603abcb4b5
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-11-02 15:28:29 +0100

    add 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('js ok');
+// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range (vedi immagine allegata):
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+
+
+const setBtn = document.getElementById('set');
+const wrapGrid = document.querySelector('.wrap-grid');
+
+// set griglia
+setBtn.addEventListener('click', () => {
+    // console.log('click');
+
+    // reset del title
+    wrapGrid.innerHTML = '';
+
+
+
+
+})
+
